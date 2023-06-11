36c450cd428b4bc34b082676890d369b6e01a88d
Jenna <jenna48071@gmail.com>
2023-06-11 18:55:39 -0500

    fixed font color on sign-in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -62,7 +62,7 @@ function SignIn() {
                 value={email}
                 onChange={onChange}
                 id="email"
-                className="input input-bordered input-primary-focus w-full "
+                className="input text-green-600 font-bold input-bordered input-primary-focus w-full "
               />
             </div>
             {/* PASSWORD */}
@@ -73,7 +73,7 @@ function SignIn() {
                 onChange={onChange}
                 id="password"
                 placeholder="Password"
-                className="input input-bordered input-primary-focus w-full "
+                className="input input-bordered text-green-600 font-bold input-primary-focus w-full "
               />
               {/* SHOW PASSWORD */}
               <div
@@ -81,9 +81,9 @@ function SignIn() {
                 onClick={() => setShowPassword(!showPassword)}
               >
                 {showPassword ? (
-                  <i className="fa-regular fa-eye-slash" />
+                  <i className="fa-regular text-green-600 font-bold fa-eye-slash" />
                 ) : (
-                  <i className="fa-regular fa-eye" />
+                  <i className="fa-regular text-green-600 font-bold fa-eye" />
                 )}
               </div>
             </div>
